08bbf93eb953ea67c788f65da87924a0ff1bc555
devimartha20 <devimarthareal@gmail.com>
2022-09-29 08:16:58 +0700

    default role 0 untuk user (migration)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->integer('role');
+            // 0 -> user; 1 -> admin
+            $table->integer('role')->default(0);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();

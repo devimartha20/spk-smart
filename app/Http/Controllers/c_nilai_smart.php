b254f7f32b8aa4c4523f4da4347e4503a14a0b8e
gusnanda7 <gus11111234@gmail.com>
2022-10-14 23:22:43 +0700

    akhir

--- app/Http/Controllers/c_nilai_smart.php
+++ app/Http/Controllers/c_nilai_smart.php
@@ -137,18 +137,18 @@ class c_nilai_smart extends Controller
         {
             $cek = $this->m_ranking->cekData();
             if ($cek->hasil_akhir <> null) {
-                $id = $data1->id;
-                $hasil_akhir = $this->m_nilai_smart->hasilData($alternative_id);
+                $m_alternatif_id = $data1->id;
+                $hasil_akhir = $this->m_nilai_smart->hasilData($m_alternatif_id);
                 $data = [
                     'hasil_akhir' => $hasil_akhir,
                 ];
                 $this->m_ranking->updateData1($id, $$data);
             } else {
-                $alternative_id = $data1->id;
-                $hasil_akhir = $this->m_nilai_smart->hasilData($alternative_id);
+                $m_alternatif_id = $data1->id;
+                $hasil_akhir = $this->m_nilai_smart->hasilData($m_alternatif_id);
                 $data = [
                     'hasil_akhir' => $hasil_akhir,
-                    'm_alternative_id' => $alternative_id,
+                    'm_alternative_id' => $m_alternatif_id,
                 ];
                 $this->m_ranking->addData($data);
             }

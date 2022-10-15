348eaf98dcfcbb3e2bfa4e6b8bcee3ccee0be19e
devimartha20 <devimarthareal@gmail.com>
2022-10-15 11:29:50 +0700

    dashboard

--- resources/views/dashboards/user/pagination/alternatif-part.blade.php
+++ resources/views/dashboards/user/pagination/alternatif-part.blade.php
@@ -47,37 +47,7 @@
   </div>
 
 @endif
-{{-- Form Tambah Alternatif --}}
-<div class="modal fade" id="tambahAlternatif" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Tambah Alternatif</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <form method="POST" action="{{ route('user.alternatif.store') }}">
-            @csrf
-            <div class="form-group">
-              <label for="recipient-name" class="col-form-label">Pilihan Jurusan :</label>
-              <input type="text" class="form-control" id="nama-alternatif" name="nama_alternatif" placeholder="Nama Jurusan" required>
-            </div>
-            <div class="form-group">
-                <label for="recipient-name" class="col-form-label">Pilihan Kampus :</label>
-                <input type="text" class="form-control" id="nama-kampus" name="nama_kampus" placeholder="Nama Kampus" required>
-              </div>
-            <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-          <button type="submit" class="btn btn-primary" value="submit">Simpan</button>
-        </div>
-    </form>
-      </div>
-    </div>
-  </div>
+
 
 
 @endsection
